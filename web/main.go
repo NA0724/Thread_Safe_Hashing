e0ea792bde37251279b81a0c938379c977df9c23
Neha Raj <neharaj710@gmail.com>
2023-01-28 11:38:40 -0800

    resolved deadlock

--- web/main.go
+++ web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -55,32 +56,40 @@ func processInput(mode string, input []string) {
 
 func manualOperation(input []string) {
 	fmt.Println("Considering thread id = 0 for Manual mode ")
-	for _, s := range input[1:] {
-		entries = append(entries, s)
+	if len(input) > 1 {
+		for _, s := range input[1:] {
+			entries = append(entries, s)
+		}
+		doManualOperation(entries)
+	} else {
+		fmt.Println("No operations were entered. Please try again")
 	}
-	doManualOperation(entries)
 }
 
 func randomOperation(input []string) {
 	var threads, operations int
 	var err error
-	// check if the number of threads and number of operations entered in an integer value
-	if threads, err = strconv.Atoi(input[1]); err == nil {
-		fmt.Println("Thread=", threads)
+	if len(input) > 3 {
+		// check if the number of threads and number of operations entered in an integer value
+		if threads, err = strconv.Atoi(input[1]); err == nil {
+			fmt.Println("Thread=", threads)
+		} else {
+			fmt.Println("Invalid format for number of threads. Please enter correct format.")
+			return
+		}
+		if operations, err = strconv.Atoi(input[2]); err == nil {
+			fmt.Println("Operations=", operations)
+		} else {
+			fmt.Println("Invalid format for number of opertions. Please enter correct format.")
+			return
+		}
+		for _, line := range input[3:] {
+			entries = append(entries, line)
+		}
+		runThreads(threads, operations, entries)
 	} else {
-		fmt.Println("Invalid format for number of threads. Please enter correct format.")
-		return
+		log.Println("Invalid number of inputs. Please try again")
 	}
-	if operations, err = strconv.Atoi(input[2]); err == nil {
-		fmt.Println("Operations=", operations)
-	} else {
-		fmt.Println("Invalid format for number of opertions. Please enter correct format.")
-		return
-	}
-	for _, line := range input[3:] {
-		entries = append(entries, line)
-	}
-	runThreads(threads, operations, entries)
 }
 
 func isRandom(mode string) bool {
